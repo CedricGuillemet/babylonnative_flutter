77ee673616a0c4407bf3c9dc85cef290b100c431
Cedric Guillemet <cedric.guillemet@gmail.com>
2022-09-20 11:56:50 +0200

    Rendering works

--- android/src/main/java/com/babylon/babylonnative/NativeView.java
+++ android/src/main/java/com/babylon/babylonnative/NativeView.java
@@ -2,6 +2,7 @@ package com.babylon.babylonnative;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -11,27 +12,29 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.graphics.Canvas;
+import android.widget.FrameLayout;
 import java.util.Map;
 
-class NativeView implements PlatformView, SurfaceHolder.Callback2, View.OnTouchListener{
-   //@NonNull private final TextView textView;
-   private final SurfaceView primarySurfaceView;
-   private final Context context;
+class NativeView extends FrameLayout implements PlatformView, SurfaceHolder.Callback2, View.OnTouchListener{
+    private static final FrameLayout.LayoutParams childViewLayoutParams = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    private final SurfaceView primarySurfaceView;
+    private final Context context;
+    private final float pixelDensityScale = getResources().getDisplayMetrics().density;
+    private boolean mViewReady = false;
 
     NativeView(@NonNull Context context, int id, @Nullable Map<String, Object> creationParams) {
-        /*textView = new TextView(context);
-        textView.setTextSize(72);
-        textView.setBackgroundColor(Color.rgb(255, 255, 255));
-        textView.setText("Rendered on a native Android view (id: " + id + ")");
-*/
-
+        super(context);
         this.primarySurfaceView = new SurfaceView(context);
-        //this.primarySurfaceView.setLayoutParams(BabylonView.childViewLayoutParams);
-        this.primarySurfaceView.getHolder().addCallback(this);
-
+        this.primarySurfaceView.setLayoutParams(NativeView.childViewLayoutParams);
         this.context = context;
-        //this.addView(this.primarySurfaceView);
 
+        SurfaceHolder holder = this.primarySurfaceView.getHolder();
+        holder.addCallback(this);
+        setOnTouchListener(this);
+
+        this.addView(this.primarySurfaceView);
+
+        setWillNotDraw(false);
 
         Wrapper.initEngine();
     }
@@ -39,7 +42,7 @@ class NativeView implements PlatformView, SurfaceHolder.Callback2, View.OnTouchL
     @NonNull
     @Override
     public View getView() {
-        return primarySurfaceView;
+        return this;
     }
 
     @Override
@@ -51,11 +54,14 @@ class NativeView implements PlatformView, SurfaceHolder.Callback2, View.OnTouchL
      */
     public void surfaceCreated(SurfaceHolder holder) {
         Wrapper.surfaceCreated(holder.getSurface(), this.context);
-        /*BabylonNative.Wrapper.setCurrentActivity(this.mCurrentActivity);
+        //BabylonNative.Wrapper.setCurrentActivity(this.mCurrentActivity);
         if (!this.mViewReady) {
-            this.mViewDelegate.onViewReady();
+            //this.mViewDelegate.onViewReady();
+            //Wrapper.loadScript("app:///Scripts/experience.js");
+            Wrapper.loadScript("app:///Scripts/marble.js");
+            Wrapper.loadScript("app:///Scripts/playground_runner.js");
             this.mViewReady = true;
-        }*/
+        }
     }
 
     /**
@@ -70,7 +76,7 @@ class NativeView implements PlatformView, SurfaceHolder.Callback2, View.OnTouchL
      * not normally called or subclassed by clients of BabylonView.
      */
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        Wrapper.surfaceChanged((int)(w / 1), (int)(h / 1), holder.getSurface());
+        Wrapper.surfaceChanged((int)(w / this.pixelDensityScale), (int)(h / this.pixelDensityScale), holder.getSurface());
     }
 
     @Override
@@ -88,7 +94,7 @@ class NativeView implements PlatformView, SurfaceHolder.Callback2, View.OnTouchL
         // Redraw happens in the bgfx thread. No need to handle it here.
     }
 
-    //@Override
+    @Override
     protected void onDraw(Canvas canvas) {
         /*if (Wrapper.isXRActive()) {
             this.xrSurfaceView.setVisibility(View.VISIBLE);
@@ -97,13 +103,14 @@ class NativeView implements PlatformView, SurfaceHolder.Callback2, View.OnTouchL
         }*/
 
         Wrapper.renderFrame();
-        //invalidate();
+        invalidate();
     }
 
+    @Override
     public boolean onTouch(View v, MotionEvent event) {
         int pointerId = event.getPointerId(event.getActionIndex());
-        float mX = event.getX(event.getActionIndex()) / 1;//this.pixelDensityScale;
-        float mY = event.getY(event.getActionIndex()) / 1;//this.pixelDensityScale;
+        float mX = event.getX(event.getActionIndex()) / this.pixelDensityScale;
+        float mY = event.getY(event.getActionIndex()) / this.pixelDensityScale;
 
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
